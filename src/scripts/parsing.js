e57035299fdc62cfe52d47ec88253ca8e59c8087
mouhamadalmounayar <muhammad.mnayar@gmail.com>
2024-07-23 17:22:10 +0200

    refactor : parsing.js

--- src/scripts/parsing.js
+++ src/scripts/parsing.js
@@ -1,39 +1,45 @@
 const { InvalidUsageError } = require("../utils/Errors");
 const containsKey = (arr, key) => arr.find((obj) => key in obj);
+const processFlag = (flag, index, commands, flags) => {
+  switch (flag) {
+    case "f":
+      if (index + 1 == commands.length) {
+        throw new InvalidUsageError("No file specified after -f flag.");
+      }
+      file = commands[index + 1];
+      flags.push({ f: file });
+      break;
+    case "a":
+      flags.push({ a: "" });
+      break;
+    default:
+      throw new InvalidUsageError(`Unknown flag -${flag}`);
+  }
+};
+
+const processScrapeFromFeed = (index, commands, flags) => {
+  if (index + 1 === commands.length) {
+    throw new InvalidUsageError(
+      "An option needs to be specified after the command scrapeFromFeed"
+    );
+  }
+  const option = commands[index + 1];
+  switch (option) {
+    case "--users":
+      flags.push({ scrapeFromFeed: "users" });
+      break;
+    default:
+      throw new InvalidUsageError(`Invalid option ${option}`);
+  }
+};
 const processCommands = (commands) => {
   let flags = [];
   for (let i = 0; i < commands.length; i++) {
     if (commands[i].startsWith("-")) {
       const flag = commands[i][1];
-
-      switch (flag) {
-        case "f":
-          if (i + 1 == commands.length) {
-            throw new InvalidUsageError("No file specified after -f flag.");
-          }
-          file = commands[i + 1];
-          flags.push({ f: file });
-          break;
-        case "a":
-          flags.push({ a: "" });
-          break;
-        default:
-          throw new InvalidUsageError(`Unknown flag -${flag}`);
-      }
+      processFlag(flag, i, commands, flags);
     } else if (commands[i] === "scrapeFromFeed") {
-      if (i + 1 === commands.length) {
-        throw new InvalidUsageError(
-          "An option needs to be specified after the command scrapeFromFeed"
-        );
-      }
-      const option = commands[i + 1];
-      switch (option) {
-        case "--users":
-          flags.push({ scrapeFromFeed: "users" });
-          break;
-        default:
-          throw new InvalidUsageError(`Invalid option ${option}`);
-      }
+      processScrapeFromFeed(i, commands, flags);
       break;
     }
   }
